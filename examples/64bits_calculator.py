af06bab5f2744a96a81fb4a353f65b6e059e70df
fG <reverser@put.as>
2012-03-05 00:26:37 +0000

    Missing stuff from last commit

---
+++ examples/64bits_calculator.py
@@ -0,0 +1,19 @@
+from pydbg import *
+import time
+import sys
+global debug
+# initialize instance
+debug = pydbg()
+# load our target
+debug.load("/Applications/Calculator.app/Contents/MacOS/Calculator", "")
+
+def handler_breakpoint (pydbg):
+ # we are responsible for deleting the breakpoint!
+ debug.bp_del(0x000000010000cc9a)
+ print "About breakpoint hit!"
+ return DBG_CONTINUE
+
+# insert a new breakpoint
+debug.bp_set(0x000000010000cc9a, handler=handler_breakpoint)
+# start
+debug.run()
