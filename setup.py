c626d21546514206e1fd307583dd2d06d04573bb
snare <snare@ho.ax>
2012-03-07 14:09:37 +1100

    Added setup.py. Should do the job of the macsetup.sh script, along with installing pydbg into the site-packages directory. Currently this builds macdll with the Debug configuration due to the Release configuration not working. Will fix that and amend setup.py.

---
+++ setup.py
@@ -0,0 +1,41 @@
+import os
+import subprocess
+from setuptools import setup
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+def build_macdll():
+    print("Building libmacdll.dylib...")
+    print("-" * 30)
+    script = """
+    cd MacOSX/macdll
+    xcodebuild -target macdll -configuration {0}
+    cp -f build/{0}/libmacdll.dylib ../../pydbg
+    cp -f build/{0}/libmacdll.dylib ../../utils
+    """
+    build_config = "Debug"
+    res = subprocess.call(script.format(build_config), shell=True)
+    print("-" * 30)
+    if res != 0:
+        print("failed to build macdll")
+        exit
+
+build_macdll()
+
+setup(
+    name = "pydbg",
+    version = "0.0.1",
+    author = "fG!",
+    author_email = "reverser@put.as",
+    description = ("Port of PyDbg to Mac OS X on x86_64"),
+    license = "GPL",
+    keywords = "pydbg debug 64-bit",
+    url = "http://github.com/gdbinit/pydbg64",
+    packages=['pydbg'],
+    install_requires = ['pydot', 'wxPython', 'mysql-python'],
+    long_description=read('README'),
+    package_data = {
+        'pydbg': ['*.dylib'],
+    },
+)
